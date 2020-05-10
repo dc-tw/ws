1123fb52a2eac86a2b5357f45c6447071fa5f55c
DC <david112358c@gmail.com>
2020-05-10 22:25:24 +0800

    bam fetch4

--- eagle.c
+++ eagle.c
@@ -1371,7 +1371,6 @@ static void process(const vector_t *var_list, FILE *out_fh)
     int refseq_length = f->seq_length;
     int count;
     char *tmp = strdup(var_data[0]->chr);
-    tmp += "\t";
     for(count = 0; count<refseq_length; ++count){
     //for(count = 0; count<100; ++count){
         if(refseq[count]=='C'){
@@ -1382,7 +1381,7 @@ static void process(const vector_t *var_list, FILE *out_fh)
     }
     var_data = (variant_t **)var_list->data;
     //qsort(var_list->data, var_list->len, sizeof(void *), nat_sort_variant);
-    //for(count=0; count<var_list->len; ++count)var_data[count]->chr = var_data[0]->chr;
+    for(count=0; count<var_list->len; ++count)var_data[count]->chr = tmp;
     /*---------*/
     print_status("new var_list->len = %d(After add all C)\n", var_list->len);
 
