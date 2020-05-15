a96f438e6fcbdb0702d3b92b8748161660e6298c
DC <david112358c@gmail.com>
2020-05-15 10:18:23 +0800

    mvh

--- eagle.c
+++ eagle.c
@@ -1059,6 +1059,27 @@ static char *evaluate(vector_t *var_set)
         char *output = malloc(sizeof(*output));
         output[0] = '\0';
         
+    if (mvh)
+    { /* Max likelihood variant hypothesis */
+        size_t max_seti = 0;
+        double r = stat[0]->mut - stat[0]->ref;
+        double has_alt = stat[0]->mut;
+        for (seti = 1; seti < stats->len; seti++)
+        {
+            if (stat[seti]->mut - stat[seti]->ref > r)
+            {
+                r = stat[seti]->mut - stat[seti]->ref;
+                has_alt = stat[seti]->mut;
+                max_seti = seti;
+            }
+        }
+        vector_t *v = vector_create(var_set->len, VARIANT_T);
+        for (i = 0; i < stat[max_seti]->combo->len; i++)
+            vector_add(v, var_data[stat[max_seti]->combo->data[i]]);
+        variant_print(&output, v, 0, stat[max_seti]->seen, stat[max_seti]->ref_count, stat[max_seti]->alt_count, log_add_exp(total, stat[max_seti]->ref), has_alt, stat[max_seti]->ref);
+        vector_free(v); //variants in var_list so don't destroy
+    }
+    else{
         for (i = 0; i < var_set->len; i++)
         {
             double has_alt = 0;
@@ -1098,6 +1119,7 @@ static char *evaluate(vector_t *var_set)
             //variant_print(&output, var_set, i, stat[seti]->seen, stat[seti]->ref_count, stat[seti]->alt_count, total, has_alt, not_alt);
             variant_print(&output, var_set, i, stat[seti]->seen, stat[seti]->ref_count, stat[seti]->alt_count, total/stats->len, has_alt, not_alt);
         }
+    }
         
         //print_status("free data\n");
         for (i = 0; i < combo->len; i++)
