5852a160caba48e783acb16285ccb3fc47af3c4e
DC <david112358c@gmail.com>
2020-05-15 22:41:09 +0800

    alt ref cnt3

--- eagle.c
+++ eagle.c
@@ -1079,7 +1079,7 @@ static char *evaluate(vector_t *var_set)
             for (i = 0; i < stat[max_seti]->combo->len; i++)
                 vector_add(v, var_data[stat[max_seti]->combo->data[i]]);
             //variant_print(&output, v, 0, stat[max_seti]->seen, stat[max_seti]->ref_count, stat[max_seti]->alt_count, log_add_exp(total, stat[max_seti]->ref), has_alt, stat[max_seti]->ref);
-            variant_print(&output, v, 0, stat[max_seti]->seen, ref_tmp, alt_tmp, log_add_exp(total, stat[max_seti]->ref), has_alt, stat[max_seti]->ref);
+            variant_print(&output, v, 0, ref_tmp+alt_tmp, ref_tmp, alt_tmp, log_add_exp(total, stat[max_seti]->ref), has_alt, stat[max_seti]->ref);
             vector_free(v); //variants in var_list so don't destroy
         }
         else{
