fd26d6b75932ce65e454932cb30a2491f56e7f8d
DC <david112358c@gmail.com>
2020-05-22 12:56:10 +0800

    may be ref6

--- eagle.c
+++ eagle.c
@@ -74,7 +74,8 @@ hts_idx_t *b_bam_idx;*/
 int ref_tmp, alt_tmp;
 char* picked_ref;
 int v_usage;
-vector_int_t *may_be_variant = vector_int_create(size_t(int));
+stats_t **use;
+vector_int_t *may_be_variant = vector_int_create(use);
 /*----------*/
 
 /* Time info */
